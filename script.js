bcc06f9ab21013b5895e5f52e17c31ea8110fb33
Sushain Gupta <89994089+SushainGupta@users.noreply.github.com>
2022-07-15 02:55:28 +0530

    added function
    added function and EventListeners to the code for the required ouput.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+/*--------------------------------
+ Function for resposive nav-bar
+ --------------------------------*/
+
 const mobile_nav = document.querySelector('.mobile-navbar-btn');
 
-const nav_header = document.querySelector('.header')
+const nav_link = document.querySelectorAll('.navbar-link'); // returns NodeList
+var div_array = [...nav_link]; // converts NodeList to Array
+div_array.forEach(navbar_link => {
+    navbar_link.addEventListener('click', () => {
+        toggleNav()
+    });
+});
+
+const nav_header = document.querySelector('.header');
 
 function toggleNav() {
     nav_header.classList.toggle("active")
@@ -8,4 +20,45 @@ function toggleNav() {
 
 mobile_nav.addEventListener('click', () => {
     toggleNav()
-})
+});
+
+
+
+/*--------------------------------------------------------------------
+Function for Hilighting navlinks on scroll to particular section
+-------------------------------------------------------------------*/
+
+
+  // Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+    // Get current scroll position
+    let scrollY = window.pageYOffset;
+  
+    // Now we loop through sections to get height, top and ID values for each
+    sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop-180;
+    const sectionId = current.getAttribute("id");
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".navbar a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".navbar a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
+
+
+
