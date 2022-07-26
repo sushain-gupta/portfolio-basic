c8be66bae69857721f8dca10e91096273983465a
Sushain Gupta <89994089+SushainGupta@users.noreply.github.com>
2022-07-27 03:37:13 +0530

    Updated script
    Updated slider function and added on click function to view project.

--- script.js
+++ script.js
@@ -216,137 +216,65 @@ function validateForm() {
 }
 
 
-        /*-------------------------------------------
-                SLIDER FUNCTION FOR PROJECT SECTION
-        ---------------------------------------------*/
+/*-------------------------------------------
+        SLIDER FUNCTION FOR PROJECT SECTION
+---------------------------------------------*/
 
-const sliderUI = {
-  slider: document.getElementById("slider"),
-  slides: document.querySelectorAll(".slide"),
-  controls: {
-    prevBtn: document.getElementById("btn-prev"),
-    nextBtn: document.getElementById("btn-next")
-  }
-};
 
-let sliderController = {
-  isMouseDown: false,
-  startPosX: 0,
-  scrollLeft: sliderUI.slider.offsetLeft,
-  goNext() {
-    let _scrollBy = Math.round((sliderUI.slider.offsetWidth + 20) - (sliderUI.slider.scrollLeft % (sliderUI.slides[0].offsetWidth + 20)));
-    
-    easyScroll({
-      scrollableDomEle: sliderUI.slider,
-      direction: "right",
-      duration: 200,
-      easingPreset: "easeInQuad",
-      scrollAmount: _scrollBy
-    });
-  },
-  goPrev() {
-    let _scrollBy = Math.round(sliderUI.slider.offsetWidth + 20) - (Math.round((sliderUI.slides[0].offsetWidth + 20)) - (sliderUI.slider.scrollLeft % (Math.round(sliderUI.slides[0].offsetWidth + 20))));
-    
-    easyScroll({
-      scrollableDomEle: sliderUI.slider,
-      direction: "left",
-      duration: 200,
-      easingPreset: "easeInQuad",
-      scrollAmount: _scrollBy
-    });
-  }
-};
+const slider = document.querySelector('.project-container');
+let isDown = false;
+let startX;
+let scrollLeft;
 
-sliderUI.controls.nextBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  sliderController.goNext();
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
-
-sliderUI.controls.prevBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  sliderController.goPrev();
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
 });
-
-sliderUI.slider.addEventListener("wheel", (event) => {
-  event.stopPropagation();
-  sliderUI.slider.scrollLeft -= event.wheelDeltaX;
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
 });
-
-sliderUI.slider.addEventListener("scroll", (event) => {
-  if (
-    sliderUI.slider.offsetWidth + sliderUI.slider.scrollLeft + 1 >
-    sliderUI.slider.scrollWidth
-  ) {
-    sliderUI.controls.nextBtn.classList.add("hide");
-    if (document.activeElement.id === sliderUI.controls.nextBtn.id) {
-      sliderUI.controls.prevBtn.focus();
-    }
-  } else {
-    sliderUI.controls.nextBtn.classList.remove("hide");
-  }
-
-  if (sliderUI.slider.scrollLeft - 1 < 0) {
-    sliderUI.controls.prevBtn.classList.add("hide");
-    if (document.activeElement.id === sliderUI.controls.prevBtn.id) {
-      sliderUI.controls.nextBtn.focus();
-    }
-  } else {
-    sliderUI.controls.prevBtn.classList.remove("hide");
-  }
-});
-
-sliderUI.slider.addEventListener("mousedown", (event) => {
-  sliderController.isMouseDown = true;
-  sliderController.scrollLeft = sliderUI.slider.scrollLeft;
-  sliderController.startPosX = event.pageX - sliderUI.slider.offsetLeft;
-});
-
-sliderUI.slider.addEventListener("mousemove", (event) => {
-  if (!sliderController.isMouseDown) return;
-  let _x = event.pageX - sliderUI.slider.offsetLeft;
-  let _xChange = _x - sliderController.startPosX;
-  sliderUI.slider.scrollLeft = sliderController.scrollLeft - _xChange;
-});
-
-sliderUI.slider.addEventListener("mouseup", (event) => {
-  sliderController.isMouseDown = false;
-});
-
-sliderUI.slider.addEventListener("mouseleave", (event) => {
-  sliderController.isMouseDown = false;
-});
-
-sliderUI.slider.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft") {
-    sliderController.goPrev();
-  }
-  if (event.key === "ArrowRight") {
-    sliderController.goNext();
-  }
-});
-
-sliderUI.slider.addEventListener("touchstart", (event) => {
-  sliderController.isMouseDown = true;
-  sliderController.scrollLeft = sliderUI.slider.scrollLeft;
-  sliderController.startPosX = event.pageX - sliderUI.slider.offsetLeft;
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
 });
 
 let images = [
   './assets/portfolio.png',
+  './assets/comming-soon.jpg',
+  './assets/comming-soon.jpg',
+  './assets/comming-soon.jpg',
+  './assets/comming-soon.jpg',
 ];
 
-sliderUI.slides.forEach((slide, index) => {
-  let img = new Image();
-  img.onload = (a) => {
-    slide.style.backgroundImage = `url(${images[index]})`;
-    slide.classList.add('has-image');
-  }
-  img.src = images[index];
+const slides = document.querySelectorAll(".slides")
+
+slides.forEach((slide, index) => {
+let img = new Image();
+img.onload = (a) => {
+  slide.style.backgroundImage = `url(${images[index]})`;
+  slide.classList.add('has-image');
+}
+img.src = images[index];
 });
 
 
-// Adding on click function to source code button in project section. 
 
-document.getElementById("src-code-btn").onclick = function () {
+// Adding on click function to source code and view project button in project section. 
+
+document.getElementById("portfolio-link-btn").onclick = function () {
+  location.href = "https://sushaingupta.netlify.app";
+}
+document.getElementById("portfolio-code-btn").onclick = function () {
   location.href = "https://github.com/SushainGupta/Portfolio";
 };
